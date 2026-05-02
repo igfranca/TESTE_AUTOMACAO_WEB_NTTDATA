be38eb8e5c90b8e6e1334c4e566c5b4c338c1a44
Igor Franca <igrfranca@gmail.com>
2026-05-02 18:55:25 -0300

    Test base pronto

--- src/test/java/br/com/nttdata/modelo/TestBase.java
+++ src/test/java/br/com/nttdata/modelo/TestBase.java
@@ -1,77 +1,68 @@
 package br.com.nttdata.modelo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class TestBase {
 
+  private static WebDriver driver;
+
+  @BeforeClass
+  public static void setUpClass() {
+    WebDriverManager.chromedriver().setup(); //Instanciar o setup do Chrome Driver por bonigarcia
+    driver = new ChromeDriver(); //Instanciar Chrome Driver
+    driver.manage().window().maximize(); //Maximizar a tela
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); //Espera implícita 20 segundos
+  }
+
   @Test
   public void testeModelo() throws InterruptedException {
-
-    WebDriverManager.chromedriver().setup(); //Instanciar o setup do Chrome Driver por bonigarcia
-    WebDriver driver = new ChromeDriver(); //Instanciar Chrome Driver
-    driver.manage().window().maximize(); //Maximizar a tela
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
     //Navegar para o site
     driver.get("https://www.petz.com.br/");
 
-    //Clicar no botão continuar e fechar
+    //Clicar no botão aceitar Cookies
     driver.findElement(By.xpath("//button[contains(text(), 'Prosseguir com todos')]")).click();
 
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-    // 1. Esperar o campo de busca ficar clicável
-    WebElement campoBusca = wait.until(
-        ExpectedConditions.elementToBeClickable(By.id("headerSearch"))
-    );
+    //Esperar 1 segundo antes de iniciar a busca
+    Thread.sleep(1000);
 
-    // 2. Clicar no campo
-    campoBusca.click();
+    //Abrir Search, digitar o item desejado e apertar Enter
+    driver.findElement(By.id("headerSearch")).sendKeys("Escada Baw & Miaw Grafite para Cães e Gatos" + Keys.ENTER);
 
-    // 3. Digitar o texto
-    campoBusca.sendKeys("Escada Baw & Miaw Grafite para Cães e Gatos");
-
-    // 4. Pressionar ENTER
-    campoBusca.sendKeys(Keys.ENTER);
-
-    // (opcional) aguardar a página de resultados carregar
-    wait.until(ExpectedConditions.urlContains("Escada"));
-
+    //Clicar no item da busca
     driver.findElement(By.xpath("//p[@class='ptz-card-label-left']")).click();
-    String valorDoProduto = driver.findElement(By.xpath("//p[@class='text-ptz-md font-bold']")).getText();
-    System.out.println("Valor do produto: "+ valorDoProduto);
 
-    // Aguarda o botão "Adicionar à sacola" ficar clicável
-    WebElement botaoAdicionar = wait.until(
-        ExpectedConditions.elementToBeClickable(
-            By.xpath("//button[contains(text(),'Adicionar à sacola')]")
-        )
-    );
+    //Pegar o valor do item na página do produto
+    String valorDoItemNaPaginaDoProduto = driver.findElement(By.xpath("//p[@class='text-ptz-md font-bold']")).getText();
 
-    // Clica no botão
-    botaoAdicionar.click();
+    //Imprimir na tela o valor do item na página do produto
+    System.out.println("Valor do item na página do produto: " + valorDoItemNaPaginaDoProduto);
 
+    //Clicar em Adicionar a sacola
+    driver.findElement(By.xpath("//div[@class='ds-button w-full']")).click();
 
+    //Clicar em ir para a sacola
+    driver.findElement(By.xpath("/html/body/div[10]/div/div[2]/div[3]/div[2]/button")).click();
 
-//    Thread.sleep(2);
-//    driver.findElement(By.xpath("//button[@data-testid='add-to-cart-button']")).click();
-//    Thread.sleep(2);
-//    driver.findElement(By.xpath("//button[@data-testid='drawer-bag-checkout-button']")).click();
+    //Pegar o valor do item na sacola
+    String valorDoItemNaSacola = driver.findElement(By.xpath("//*[@id='cart-item-164109']/div/div[1]/div/div[2]/div[1]/div")).getText();
 
+    //Imprimir na tela o valor do item na sacola
+    System.out.println("Valor do item na sacola: " + valorDoItemNaSacola);
 
+  }
 
-    //driver.quit(); // feche se quiser
-
-
-
+  @AfterClass
+  public static void tearDownClass() {
+    driver.quit(); //Fechar o navegador e encerrar o Chrome Driver
   }
 }
